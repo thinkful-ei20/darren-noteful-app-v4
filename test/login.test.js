8218e61d5e480021c0b8c66f030812c3c2df95d6
Darren <ninenumbproductions@gmail.com>
2018-05-10 12:18:40 -0700

    jwt refresh tests success

--- test/login.test.js
+++ test/login.test.js
@@ -44,55 +44,123 @@ describe.only('Noteful API - login', function() {
     return mongoose.disconnect();
   });
 
-  it('Should return a valid auth token', function () {
-    return chai.request(app)
-      .post('/api/login')
-      .send({ username, password })
-      .then(res => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('object');
-        expect(res.body.authToken).to.be.a('string');
-  
-        const payload = jwt.verify(res.body.authToken, JWT_SECRET);
-  
-        expect(payload.user).to.not.have.property('password');
-        expect(payload.user.username).to.equal(username);
-        expect(payload.user.fullname).to.equal(fullname);
-      });
-  });
 
-  it('Should reject requests with no credentials',function () {
-    return chai.request(app)
-      .post('/api/login')
-      .send({})
-      .then(res => {
+
+  describe('login post w/ Token', function() {
+    it('Should return a valid auth token', function () {
+      return chai.request(app)
+        .post('/api/login')
+        .send({ username, password })
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.authToken).to.be.a('string');
+  
+          const payload = jwt.verify(res.body.authToken, JWT_SECRET);
+  
+          expect(payload.user).to.not.have.property('password');
+          expect(payload.user.username).to.equal(username);
+          expect(payload.user.fullname).to.equal(fullname);
+        });
+    });
+
+    it('Should reject requests with no credentials',function () {
+      return chai.request(app)
+        .post('/api/login')
+        .send({})
+        .then(res => {
         // console.log(res);
-        expect(res).to.have.status(400);
-        expect(res.body.message).to.equal('Bad Request');
-        expect(res.body).to.not.have.property('authToken');
+          expect(res).to.have.status(400);
+          expect(res.body.message).to.equal('Bad Request');
+          expect(res.body).to.not.have.property('authToken');
 
       
-      });
+        });
+    });
+
+    it('Should reject requests with incorrect usernames', function() {
+      return chai.request(app)
+        .post('/api/login')
+        .send({username: 'badUser', fullname, password})
+        .then(res => {
+          expect(res).to.have.status(401);
+          expect(res.body.message).to.equal('Unauthorized');        
+        });
+    });
+
+    it('Should reject requests with incorrect passwords',function() {
+      return chai.request(app)
+        .post('/api/login')
+        .send({username, fullname, password: 'passwordBad'})
+        .then(res => {
+          expect(res).to.have.status(401);
+          expect(res.body.message).to.equal('Unauthorized');        
+        });
+    });
   });
 
-  it('Should reject requests with incorrect usernames', function() {
-    return chai.request(app)
-      .post('/api/login')
-      .send({username: 'badUser', fullname, password})
-      .then(res => {
-        expect(res).to.have.status(401);
-        expect(res.body.message).to.equal('Unauthorized');        
-      });
-  });
+  describe('refresh auth token', function () {
 
-  it('Should reject requests with incorrect passwords',function() {
-    return chai.request(app)
-      .post('/api/login')
-      .send({username, fullname, password: 'passwordBad'})
-      .then(res => {
-        expect(res).to.have.status(401);
-        expect(res.body.message).to.equal('Unauthorized');        
-      });
+    it('should return a valid auth token with a newer expiry date', function() {
+      const user = {username, password};
+      const token = jwt.sign({ user }, JWT_SECRET, {subject: username, expiresIn: '1m'});
+      const decoded = jwt.decode(token);
+
+      return chai.request(app)
+        .post('/api/refresh')
+        .set('Authorization', `Bearer ${token}`)
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.a('object');
+          const authToken = res.body.authToken;
+          expect(authToken).to.be.a('string');
+
+          const payload = jwt.verify(authToken, JWT_SECRET);
+          
+          expect(payload.user).to.deep.equal({ username, password });
+          expect(payload.exp).to.be.greaterThan(decoded.exp);
+        });
+    });
+
+    it('should reject requests with no credentials',function() {
+
+      return chai.request(app)
+        .post('/api/refresh')
+        .then(res => {
+          expect(res).to.have.status(401);
+          expect(res.body.authToken).to.not.exist;
+        });  
+    });
+
+    it('should reject requests with an invalid token', function () {
+      const token = 'gobblygook122@';
+
+      return chai.request(app)
+        .post('/api/refresh')
+        .set('Authorization', `Bearer ${token}`)
+        .then(res => {
+          expect(res).to.have.status(401);
+          expect(res.body.message).to.be.equal('Unauthorized'); 
+        });
+    });
+
+    it('should reject requests with an expired token', function () {
+      const user = {username, password};
+      const token = jwt.sign({ user }, JWT_SECRET, {subject: username, expiresIn: '100ms'});
+
+      return chai.request(app)
+        .post('/api/refresh')
+        .set('Authorization', `Bearer ${token}`)
+        .then(res => {
+          expect(res).to.have.status(401);
+          expect(res.body).to.be.a('object');
+          const authToken = res.body.authToken;
+          expect(authToken).to.not.exist;
+        });
+    });
+
+
+
   });
   
 
